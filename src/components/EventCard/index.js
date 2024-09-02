6bd3be5621b0bca00f30e59ed67b100908a9d75c
RudiDonnot <149707597+RudiDonnot@users.noreply.github.com>
2024-09-02 21:29:33 +0200

    with eventcards

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -11,7 +11,22 @@ const EventCard = ({
   label,
   small = false,
   ...props
-};
+}) => (
+  <div
+    data-testid="card-testid"
+    className={`EventCard${small ? " EventCard--small" : ""}`}
+    {...props}
+  >
+    <div className="EventCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+      <div className="EventCard__label">{label}</div>
+    </div>
+    <div className="EventCard__descriptionContainer">
+      <div className="EventCard__title">{title}</div>
+      <div className="EventCard__month">{getMonth(date)}</div>
+    </div>
+  </div>
+);
 
 EventCard.propTypes = {
   imageSrc: PropTypes.string,
